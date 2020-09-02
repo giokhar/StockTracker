129374ab8450905a92c5d0cd419df272f2da9f87
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2020-09-02 01:18:42 -0400

    net gain calculator

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 from flask_caching import Cache
-from helpers import api_request, get_datetime_old, get_datetime_now, get_cache_key, beautify_log
+from helpers import api_request, get_datetime_old, get_datetime_now, get_cache_key, beautify_log, get_net_gain
 from helpers import MINUTE, DAY
 from database import Database
 
@@ -10,31 +10,35 @@ db = Database()
 
 
 @app.route('/')
+@cache.cached(timeout=MINUTE)
 def index():
     log = db.get_log_for_portfolio(1)
+    total_gain = 0
+    for stock in log:
+        current = get_stock_price(stock['ticker'])['c']
+        total_gain += get_net_gain(current, stock['price'], stock['shares'])
     return render_template("index.html", data={
-        "log": log
+        "total_gain": total_gain
     })
 
 
-@app.route('/api/stock/<string:symbol>', methods=['GET'])
-@cache.cached(timeout=MINUTE)
-def get_stock_price(symbol):
-    result = api_request(f"/quote?symbol={symbol}")
+@app.route('/api/stock/<string:ticker>', methods=['GET'])
+def get_stock_price(ticker):
+    result = api_request(f"/quote?symbol={ticker}")
     return result
 
 
-@app.route('/api/stock/history/<string:symbol>', methods=['GET'])
-def get_stock_details(symbol):
-    json = get_stock_price_log(symbol)
+@app.route('/api/stock/history/<string:ticker>', methods=['GET'])
+def get_stock_details(ticker):
+    json = get_stock_price_log(ticker)
     result = beautify_log(json, 'W', 100)
     return jsonify(result)
 
 
 @cache.cached(timeout=DAY, key_prefix=get_cache_key)
-def get_stock_price_log(symbol):
+def get_stock_price_log(ticker):
     """Get historical data and cache it for 24 hrs"""
-    json = api_request(f"/stock/candle?symbol={symbol}&resolution=60&from={get_datetime_old(1).timestamp()}&to={get_datetime_now().timestamp()}")
+    json = api_request(f"/stock/candle?symbol={ticker}&resolution=60&from={get_datetime_old(1).timestamp()}&to={get_datetime_now().timestamp()}")
     return json
 
 
